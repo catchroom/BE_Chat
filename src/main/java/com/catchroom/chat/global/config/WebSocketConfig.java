b43eac2c87452d957b04342e7cdf6977553fcf1e
sungjiwoon <nebulaisme@naver.com>
2024-01-12 17:29:15 +0900

    WebSocketConfig 원상복귀

--- src/main/java/com/catchroom/chat/global/config/WebSocketConfig.java
+++ src/main/java/com/catchroom/chat/global/config/WebSocketConfig.java
@@ -25,7 +25,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp")
-                .setAllowedOrigins("*")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
