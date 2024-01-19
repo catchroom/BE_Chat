b40749a55e6c2f663865488f9973bdde157566ce
sungjiwoon <nebulaisme@naver.com>
2024-01-19 15:52:03 +0900

    Feat : 마지막 메시지 저장

--- src/main/java/com/catchroom/chat/message/repository/ChatRoomRepository.java
+++ src/main/java/com/catchroom/chat/message/repository/ChatRoomRepository.java
@@ -1,5 +1,6 @@
 package com.catchroom.chat.message.repository;
 
+import com.catchroom.chat.message.dto.ChatMessageDto;
 import com.catchroom.chat.message.dto.ChatRoom;
 import jakarta.annotation.Resource;
 import java.util.List;
@@ -9,18 +10,20 @@ import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
+/** redis 와 관련된 메소드들 */
+
 @RequiredArgsConstructor
 @Service
 public class ChatRoomRepository {
-    // Redis CacheKeys
-    private static final String CHAT_ROOMS = "CHAT_ROOM_REDIS";
+
+    private static final String CHAT_ROOM = "CHAT_ROOM_LAST_MSG"; //채팅방
     public static final String USER_COUNT = "USER_COUNT"; // 채팅룸에 입장한 클라이언트수 저장
     public static final String ENTER_INFO = "ENTER_INFO"; // 채팅룸에 입장한 클라이언트의 sessionId와 채팅룸 id를 맵핑한 정보 저장
 
     // HashOperations<String, String, ChatRoom> : Redis의 해시 데이터 구조를 다룸.
     // String 타입의 key, String 타입의 필드, chatRoom 객체의 값으로 구성된 해시를 다룬다.
     @Resource(name = "redisTemplate") //redisTemplate bean 주입.
-    private HashOperations<String, String, ChatRoom> opsHashChatRoom;
+    private HashOperations<String, String, ChatMessageDto> opsHashLastChatMessage;
 
     @Resource(name = "redisTemplate")
     private HashOperations<String, String, String> hashOpsEnterInfo;
@@ -31,27 +34,14 @@ public class ChatRoomRepository {
     @Resource(name = "redisTemplate")
     private ValueOperations<String, String> userInfoOps;
 
-    // 모든 채팅방 조회
-    public List<ChatRoom> findAllRoom() {
-        return opsHashChatRoom.values(CHAT_ROOMS);
+    public void setLastChatMessage(String roomId, ChatMessageDto chatMessageDto) {
+        opsHashLastChatMessage.put(CHAT_ROOM, roomId, chatMessageDto);
     }
 
-    // 특정 채팅방 조회
-    public ChatRoom findRoomById(String id) {
-        return opsHashChatRoom.get(CHAT_ROOMS, id);
+    public ChatMessageDto getLastMessage(String roomId) {
+        return opsHashLastChatMessage.get(CHAT_ROOM, roomId);
     }
 
-    // 채팅방 생성 : 서버간 채팅방 공유를 위해 redis hash에 저장한다.
-    public ChatRoom createChatRoom(String name) {
-        ChatRoom chatRoom = ChatRoom.create(name);
-        opsHashChatRoom.put(CHAT_ROOMS, chatRoom.getRoomId(), chatRoom);
-        return chatRoom;
-    }
-
-    // 세션 id와 유저 정보 저장 .
-    public void setUserInfoBySessionId(String sessionId, String name) {
-        if (userInfoOps.get(sessionId) == null) userInfoOps.append(sessionId, name);
-    }
 
     // 세션 id와 유저 정보 가져오기.
     public String getUserInfoBySessionId(String sessionId) {
