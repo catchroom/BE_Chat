84f25a2099dd04303bace8e436ac9090ae4c942e
sungjiwoon <nebulaisme@naver.com>
2024-01-23 14:21:56 +0900

    feat : 채팅방 수정

--- src/main/java/com/catchroom/chat/feign/client/MainFeignClient.java
+++ src/main/java/com/catchroom/chat/feign/client/MainFeignClient.java
@@ -20,12 +20,8 @@ import org.springframework.web.bind.annotation.RequestParam;
  *
  *
  */
-//@FeignClient(
-//        name = "mainFeign", url = "https://catchroom.xyz/v1",
-//        configuration = FeignConfig.class
-//)
 @FeignClient(
-        name = "mainFeign", url = "http://localhost:8080/v1",
+        name = "mainFeign", url = "https://catchroom.xyz/v1",
         configuration = FeignConfig.class
 )
 public interface MainFeignClient {
