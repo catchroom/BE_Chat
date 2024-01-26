7520b660c1071cbc91ba2be963e4417229863bf7
sungjiwoon <nebulaisme@naver.com>
2024-01-26 19:52:53 +0900

    feat : exceptionHandler 처리

--- src/main/java/com/catchroom/chat/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/catchroom/chat/global/exception/GlobalExceptionHandler.java
@@ -2,15 +2,18 @@ package com.catchroom.chat.global.exception;
 
 import com.catchroom.chat.global.common.SuccessMessage;
 import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<SuccessMessage> handleException(Exception e) {
+        log.error(e.getMessage());
         return ResponseEntity.badRequest().body(
                 SuccessMessage.createSuccessMessage("ERROR")
         );
@@ -18,6 +21,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(FeignException.class)
     public ResponseEntity<SuccessMessage> handleFeignException(FeignException e) {
+        log.error("존재하지 않는 엑세스 토큰입니다. ");
         return ResponseEntity.badRequest().body(
                 SuccessMessage.createSuccessMessage("TOKEN ERROR")
         );
