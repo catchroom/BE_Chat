a12e93d4a4a602fa747b6f7ef5486617cae9fc89
sungjiwoon <nebulaisme@naver.com>
2024-01-26 19:51:19 +0900

    feat : exceptionHandler 처리

--- src/main/java/com/catchroom/chat/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/catchroom/chat/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.catchroom.chat.global.exception;
 
 import com.catchroom.chat.global.common.SuccessMessage;
+import feign.FeignException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,9 +10,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<SuccessMessage> handleBaseException(Exception e) {
+    public ResponseEntity<SuccessMessage> handleException(Exception e) {
         return ResponseEntity.badRequest().body(
                 SuccessMessage.createSuccessMessage("ERROR")
+        );
+    }
+
+    @ExceptionHandler(FeignException.class)
+    public ResponseEntity<SuccessMessage> handleFeignException(FeignException e) {
+        return ResponseEntity.badRequest().body(
+                SuccessMessage.createSuccessMessage("TOKEN ERROR")
         );
     }
 }
